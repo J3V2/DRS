8413dbb34a0917a2821dcfc8f72467b6027bee27
J3V2 <150044457+J3V2@users.noreply.github.com>
2024-05-18 13:32:46 +0800

    Fixed Vite Issuer regarding Vite Manifest in npm run build command

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/css/user.css",
+                "resources/css/admin.css",
+                "resources/js/app.js",
+                "resources/js/user.js",
+                "resources/js/admin.js",
+                "resources/js/bootstrap.js",
+            ],
             refresh: true,
         }),
     ],
